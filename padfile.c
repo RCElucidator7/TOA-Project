ca08a0a41f3026f64711bb51b0db8cdcca42362c
Ryan Conway <rmconway27@gmail.com>
2019-03-25 16:33:00 +0000

    Reading files as bytes

--- padfile.c
+++ padfile.c
@@ -1,16 +1,26 @@
 #include<stdio.h>
 #include<stdint.h>
 
+union messageblock {
+    uint8_t b[64];
+    uint32_t mb[16];
+    uint64_t pad[8];
+};
 
 int main(int argc, char *argv[]){
 
-    FILE* f;
+    union messageblock MB;
 
-    char c;
+    uint64_t nobytes;
+
+    FILE* f;
 
     f = fopen(argv[1], "r");
 
-    printf("The character is: %c\n", fread(&c, 1, 1, f));
+    while (!feof(f)){
+	nobytes = fread(MB.b, 1, 64, f);
+	printf("%llu \n", nobytes);
+    }
 
     fclose(f);
     return 0;
