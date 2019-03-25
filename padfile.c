1ef390442c8bfd8ffdda673878526617c27c195f
Ryan Conway <rmconway27@gmail.com>
2019-03-25 17:07:15 +0000

    Block is being padded

--- padfile.c
+++ padfile.c
@@ -13,15 +13,36 @@ int main(int argc, char *argv[]){
 
     uint64_t nobytes;
 
+    //Padder
+    uint64_t nobits = 0;
+
     FILE* f;
 
     f = fopen(argv[1], "r");
 
     while (!feof(f)){
 	nobytes = fread(MB.b, 1, 64, f);
-	printf("%llu \n", nobytes);
+	nobits = nobits + (nobytes * 8);
+	if(nobytes < 56){
+	    printf("Block with less than 55 bytes found.\n");
+	    //1 is on the right
+	    MB.b[nobytes] = 0x01;
+	    //1 is on the left
+	    //MB.b[nobytes] = 0x80;
+	    while (nobytes < 56){
+	        nobytes = nobytes + 1;
+		MB.b[nobytes] = 0x00;
+	    }
+	    MB.pad[7] = nobits;
+	}
     }
 
     fclose(f);
+    
+    for(int i = 0; i < 64; i++){
+	printf("%x ", MB.b[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
