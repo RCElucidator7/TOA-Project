565e45e6425f6c417adf36eaec6376b0adfce9d8
Ryan Conway <rmconway27@gmail.com>
2019-03-25 19:04:18 +0000

    Finished Padding file

--- padfile.c
+++ padfile.c
@@ -7,34 +7,60 @@ union messageblock {
     uint64_t pad[8];
 };
 
+enum status {READ, PAD0, PAD1, FINISH};
+
 int main(int argc, char *argv[]){
 
     union messageblock MB;
 
+    //Number of bytes returned
     uint64_t nobytes;
 
-    //Padder
+    //Keep track of bits
     uint64_t nobits = 0;
+
+    enum status S = READ;
 
     FILE* f;
 
     f = fopen(argv[1], "r");
 
-    while (!feof(f)){
+    while (S == READ){
 	nobytes = fread(MB.b, 1, 64, f);
 	nobits = nobits + (nobytes * 8);
 	if(nobytes < 56){
 	    printf("Block with less than 55 bytes found.\n");
-	    //1 is on the right
-	    MB.b[nobytes] = 0x01;
-	    //1 is on the left
-	    //MB.b[nobytes] = 0x80;
+	    //Maybe read in an option for little or big edian
+	    //1 is on the right (Big edian)
+	    // MB.b[nobytes] = 0x01;
+	    //1 is on the left (Little edian)
+	    MB.b[nobytes] = 0x80;
 	    while (nobytes < 56){
 	        nobytes = nobytes + 1;
 		MB.b[nobytes] = 0x00;
 	    }
 	    MB.pad[7] = nobits;
+	    S = FINISH;
+	} else if (nobytes < 64) {
+	    S = PAD0;
+	    MB.b[nobytes] = 0x80;
+	    while (nobytes < 64){
+		nobytes = nobytes + 1;
+		MB.b[nobytes] = 0x00;
+	    }
+	} else if (feof(f)) {
+	    S = PAD1;
 	}
+    }
+
+    if (S == PAD0 || S == PAD1){
+	for(int j = 0; j < 56; j++){
+	    MB.b[j] = 0x00;
+	}
+	MB.pad[7] = nobits;
+    }
+    if (S == PAD1){
+	MB.mb[0] = 0x80;
     }
 
     fclose(f);
