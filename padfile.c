7603df4a9214ffe2dffce5004d4a83296a907853
Ryan Conway <rmconway27@gmail.com>
2019-03-25 19:56:45 +0000

    Added comments to the padfile

--- padfile.c
+++ padfile.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 #include<stdint.h>
 
+//Union defined messageblock to store different data types
 union messageblock {
     uint8_t b[64];
     uint32_t mb[16];
     uint64_t pad[8];
 };
 
+//Enum to keep track of what stage the file is in
 enum status {READ, PAD0, PAD1, FINISH};
 
+//Main method
 int main(int argc, char *argv[]){
 
     union messageblock MB;
@@ -21,13 +24,18 @@ int main(int argc, char *argv[]){
 
     enum status S = READ;
 
+    //File variable
     FILE* f;
 
+    //Open the file and read it in
     f = fopen(argv[1], "r");
 
+    //While the file is being read
     while (S == READ){
+	//Read in a block of data from stream and assign to variable
 	nobytes = fread(MB.b, 1, 64, f);
 	nobits = nobits + (nobytes * 8);
+	//If a block with less than 55 bytes is found, apply the padding
 	if(nobytes < 56){
 	    printf("Block with less than 55 bytes found.\n");
 	    //Maybe read in an option for little or big edian
@@ -63,8 +71,10 @@ int main(int argc, char *argv[]){
 	MB.mb[0] = 0x80;
     }
 
+    //Close the file
     fclose(f);
     
+    //Loop through the bytes in the message block abd print them to the console
     for(int i = 0; i < 64; i++){
 	printf("%x ", MB.b[i]);
     }
